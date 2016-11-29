3ea4ce3da05921fab198534fd55903f51de461d1
Carmen Chico <CarmenChico@icloud.com>
2016-11-29 14:56:43 -0500

    added functions to feed/pet

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,12 @@
-function Dog () {
+function Dog (options) {
+  let opts = options || {};
+
+  this.status = "normal" || "happy";
+  this.color = opts.color|| "black";
+  this.hungry = opts.hungry || "true";
+
 }
+
+
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,15 @@
-function Person () {
-};
+function Person (options) {
+	let opts = options || {};
 
+	this.name = opts.name;
+}
+//let mason = new Person ({name: "mason"});
+
+Person.prototype.pet = function () {
+
+}
+Person.prototype.feed = function () {
+
+}
+;
 export { Person };
