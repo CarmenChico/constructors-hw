d02ed051ffce30134b7c8229a7f0bd869fbfec97
Carmen Chico <CarmenChico@icloud.com>
2016-11-29 16:54:54 -0500

    normal mode:tests passed

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -3,10 +3,12 @@ function Dog (options) {
 
   // this.status = "normal" || "happy";  -- this is not the route that needs to be used.
   this.status = opts.status || "normal";
+    // this.status = opts.status; -- tried to keep normal out of it just for the hec of it.
   this.color = opts.color;
-  this.hungry = opts.hungry || false;
-  // if (this.hungry == false ) {return true;
-// }
-}
+  // this.hungry = opts.hungry || false;
+  if (opts.hungry === false) {this.hungry=false}
+  else {this.hungry = true};
+};
+
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -6,7 +6,7 @@ function Person (options) {
 	else {this.cool = false};
 };
 //let mason = new Person ({name: "mason"});
-
+//added these 2 functions becuase they were being called and were mising ..pet ..feed... which was making the other test results not appear.
 Person.prototype.pet = function (dog) {
 	dog.status = "happy";
 }
