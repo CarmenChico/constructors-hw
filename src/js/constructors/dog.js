c5a5224fe4f009abbbac80e9333f4b9659b604ce
Carmen Chico <CarmenChico@icloud.com>
2016-11-29 16:45:25 -0500

    more tests passed

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,12 +1,12 @@
 function Dog (options) {
   let opts = options || {};
 
-  this.status = "normal" || "happy";
-  this.color = opts.color|| "black";
-  this.hungry = opts.hungry || "true";
-
+  // this.status = "normal" || "happy";  -- this is not the route that needs to be used.
+  this.status = opts.status || "normal";
+  this.color = opts.color;
+  this.hungry = opts.hungry || false;
+  // if (this.hungry == false ) {return true;
+// }
 }
-
-
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -2,14 +2,16 @@ function Person (options) {
 	let opts = options || {};
 
 	this.name = opts.name;
-}
+	if (opts.cool === true) {this.cool = true}
+	else {this.cool = false};
+};
 //let mason = new Person ({name: "mason"});
 
-Person.prototype.pet = function () {
-
+Person.prototype.pet = function (dog) {
+	dog.status = "happy";
 }
-Person.prototype.feed = function () {
-
+Person.prototype.feed = function (dog) {
+ dog.hungry = false;
 }
 ;
 export { Person };
